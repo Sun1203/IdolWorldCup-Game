7654f6acb77216c3170b9cdec416e41b2afb04ae
Sun1203 <84012715+Sun1203@users.noreply.github.com>
2022-09-10 17:54:34 +0900

    버튼 및 이벤트 적용

--- IdolWorldCup/src/IdolWorldCup.java
+++ IdolWorldCup/src/IdolWorldCup.java
@@ -22,7 +22,7 @@ public class IdolWorldCup {
 	
 	// function : image to put on Button Control
 	static ImageIcon changeImage(String filename) {
-		ImageIcon icon = new ImageIcon("./images" + filename);
+		ImageIcon icon = new ImageIcon("./images/" + filename);
 		Image originImage = icon.getImage();
 		Image changedImage = originImage.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 		ImageIcon icon_new = new ImageIcon(changedImage);
@@ -44,7 +44,31 @@ public class IdolWorldCup {
 			labelMessage = new JLabel("Find Your Idol!!");
 			labelMessage.setFont(new Font("Arial", Font.BOLD, 20));
 			panelNorth.add(labelMessage);
-			this.add( panelNorth );
+			this.add( "North", panelNorth );
+			
+			panelCenter = new JPanel();
+			labelVs = new JLabel("vs");
+			labelVs.setFont(new Font("Arial", Font.BOLD, 20));
+			buttonLeft = new JButton("LeftButton");
+			buttonLeft.setIcon(changeImage("idol1.png"));
+			buttonLeft.setPreferredSize(new Dimension(200, 200));
+			
+			buttonRight = new JButton("RightButton");
+			buttonRight.setIcon(changeImage("idol2.png"));
+			buttonRight.setPreferredSize(new Dimension(200, 200));
+			
+			//add click event
+			buttonLeft.addActionListener(this);
+			buttonRight.addActionListener(this);
+			
+			panelCenter.add(buttonLeft);
+			panelCenter.add(labelVs);
+			panelCenter.add(buttonRight);
+			
+			this.add( "Center", panelCenter );
+			
+			this.pack(); // clear empty space(빈 공간을 없애줌)
+			
 			
 		}
 		
@@ -52,6 +76,32 @@ public class IdolWorldCup {
 		// Button Click Event Callback
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			if (imageIndex == 16) {
+				// Last Idol
+				labelMessage.setText("Found Your Idol!");
+				// show Last Image
+				if ( e.getActionCommand().equals("LeftButton")) {
+					buttonRight.hide();
+					labelVs.hide();
+				}
+				else {
+					buttonLeft.hide();
+					labelVs.hide();
+				}
+			}
+			else {
+				if ( e.getActionCommand().equals("LeftButton")) {
+					// LeftButton
+					buttonRight.setIcon(changeImage(images[imageIndex]));
+					imageIndex++;
+				}
+				else {
+					// Right Button
+					buttonLeft.setIcon(changeImage(images[imageIndex]));
+					imageIndex++;
+					
+				}
+			}
 			
 		}
 	}
